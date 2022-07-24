c18262a2d122f7cc56abf775b787f50fd99abe26
Andro76as <andro76as@gmail.com>
2022-07-24 16:10:32 +0300

    HTTP-and-WEB 1.2

--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,18 +1,20 @@
 package ru.netology;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+
 public class Server {
+    private final Map<String, Map<String, Handler>> handlersMap = new ConcurrentHashMap<>();
     private final int port;
-    final ExecutorService threadPool = Executors.newFixedThreadPool(64); //указываем фиксированный пул потоков
-    //У нас есть Executor для execute (т.е. выполнения) некой задачи в потоке, когда реализация создания потока
-    // скрыта от нас. У нас есть ExecutorService — особый Executor, который имеет набор возможностей по управлению
-    // ходом выполнения
+    final ExecutorService threadPool = Executors.newFixedThreadPool(64);
+
     public Server(int port) {
         this.port = port;
     }
@@ -24,15 +26,42 @@ public class Server {
             while (true) {
                 try {
                     final Socket socket = serverSocket.accept();
-                    Runnable serverRunnable = new ServerRunnable(socket, validPaths);
+                    Runnable serverRunnable = new ServerRunnable(socket, validPaths, this);
                     threadPool.submit(serverRunnable);
-                    //отправляем задачи на выполнение при помощи submit
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void addHandler(String method, String path, Handler handler) {
+        Map<String, Handler> pathMap = handlersMap.get(method);
+        if (null == pathMap) {
+            pathMap = new ConcurrentHashMap<>();
+            pathMap.put(path, handler);
+            handlersMap.put(method, pathMap);
+            return;
+        }
+        pathMap.putIfAbsent(path, handler);
+    }
+
+    public Handler getHandler(String method, String path) {
+        Map<String, Handler> pathMap = handlersMap.get(method);
+        if (null == pathMap) return null;
+        return pathMap.get(path);
+    }
+
+    public void printHandlers() {
+        System.out.println("Список добавленных хэндлеров: ");
+        for (Map.Entry<String, Map<String, Handler>> entry : handlersMap.entrySet()) {
+            System.out.print(entry.getKey() + ": ");
+            for (Map.Entry<String, Handler> innerEntry : entry.getValue().entrySet()) {
+                System.out.print(innerEntry.getKey() + "; ");
+            }
+            System.out.println();
         }
     }
 }
